4af8d4334357447cb4bb6b3ccc36896a3d5fa18d
owolabiadebayo <owolabiadebayo@punch.cool>
2023-10-10 11:38:00 +0100

    update

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,52 +1,141 @@
-import React from 'react'
-import abuja from './images/abuja-destination.png'
-import akure from './images/akure-destination.jpg'
-import pharcourt from './images/portharcourt-destination.png'
-import './App.css'
+import React from "react";
+import abuja from "./images/abuja-destination.png";
+import akure from "./images/akure-destination.jpg";
+import pharcourt from "./images/portharcourt-destination.png";
+import "./App.css";
 
 function Carousel() {
   return (
- <>
-     <div class="container mx-auto my-5 bg-light rounded p-5" style={{width: '80%', backgroundColor:'#ffff'}}>
-      <h2>Our <span style={{borderBottom:'1px solid purple' }}>Destinations</span></h2>
-  <div class="row align-items-center mt-3">
+    <>
+      <div
+        class="container mx-auto my-5 bg-light rounded p-5"
+        style={{ width: "80%", backgroundColor: "#ffff" }}
+      >
+        <h2>
+          Our{" "}
+          <span style={{ borderBottom: "1px solid purple" }}>Destinations</span>
+        </h2>
+        <div class="row align-items-center mt-3">
+          <div class="col-lg-6">
+            <div
+              id="carouselExampleFade"
+              class="carousel slide carousel-fade"
+              data-bs-ride="carousel"
+              data-bs-interval="5000"
+            >
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src={pharcourt} class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src={abuja} class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src={akure} class="d-block w-100" alt="..." />
+                </div>
+              </div>
+              <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleFade"
+                data-bs-slide="prev"
+              >
+                <span
+                  class="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button
+                class="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleFade"
+                data-bs-slide="next"
+              >
+                <span
+                  class="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
 
-    <div class="col-lg-6">
-      <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src={pharcourt} class="d-block w-100" alt="..."/>
-          </div>
-          <div class="carousel-item">
-            <img src={abuja} class="d-block w-100" alt="..."/>
-          </div>
-          <div class="carousel-item">
-            <img src={akure} class="d-block w-100" alt="..."/>
+          <div class="col-lg-6">
+            <p>
+              Your paragraph of text goes here. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Sed id velit justo. Curabitur
+              efficitur, lectus sit amet ultricies tincidunt, libero leo
+              ultricies ligula, vel laoreet arcu est vel quam. Nulla facilisi.
+            </p>
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
-    </div>
-
-  
-    <div class="col-lg-6">
-      <p>
-        Your paragraph of text goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id velit justo.
-        Curabitur efficitur, lectus sit amet ultricies tincidunt, libero leo ultricies ligula, vel laoreet arcu est vel
-        quam. Nulla facilisi.
-      </p>
-    </div>
-  </div>
+      <div
+        class="container mx-auto my-5 bg-light rounded p-5"
+        style={{ width: "80%", backgroundColor: "#ffff" }}
+      >
+        <h2>
+          Our{" "}
+          <span style={{ borderBottom: "1px solid purple" }}>Destinations</span>
+        </h2>
+        <div class="row align-items-center mt-3">
+          <div class="col-lg-6">
+            <p>
+              Your paragraph of text goes here. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Sed id velit justo. Curabitur
+              efficitur, lectus sit amet ultricies tincidunt, libero leo
+              ultricies ligula, vel laoreet arcu est vel quam. Nulla facilisi.
+            </p>
+          </div>
+          <div class="col-lg-6">
+            <div
+              id="carouselExampleFade"
+              class="carousel slide carousel-fade"
+              data-bs-ride="carousel"
+              data-bs-interval="5000"
+            >
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src={pharcourt} class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src={abuja} class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src={akure} class="d-block w-100" alt="..." />
+                </div>
+              </div>
+              <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleFade"
+                data-bs-slide="prev"
+              >
+                <span
+                  class="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button
+                class="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleFade"
+                data-bs-slide="next"
+              >
+                <span
+                  class="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
         </div>
- </>
-  )
+      </div>
+    </>
+  );
 }
 
-export default Carousel
+export default Carousel;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,168 +1,232 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Checkin from './Checkin'
-import './App.css'
-import './Custom.scss'
+import "./Navbar.css";
+import Checkin from "./Checkin";
+import "./App.css";
+import "./Custom.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlane } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
-    const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [bookingRef, setBookingRef] = useState("");
+  const [contactEmail, setContactEmail] = useState("");
 
-    useEffect(() => {
-      const handleScroll = () => {
-        const st = window.pageYOffset || document.documentElement.scrollTop;
-        if (st > lastScrollTop) {
-          // Scroll down
-          document.querySelector('.navbar').style.top = "-70px"; // Adjust the value as needed
-        } else {
-          // Scroll up
-          document.querySelector('.navbar').style.top = "0";
-        }
-        setLastScrollTop(st <= 0 ? 0 : st);
-      };
-  
-      window.addEventListener("scroll", handleScroll);
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    }, [lastScrollTop]);
+  const handleBookingRefChange = (e) => {
+    setBookingRef(e.target.value);
+  };
+
+  const handleContactEmailChange = (e) => {
+    setContactEmail(e.target.value);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const st = window.pageYOffset || document.documentElement.scrollTop;
+      if (st > lastScrollTop) {
+        // Scroll down
+        document.querySelector(".navbar").style.top = "-70px"; // Adjust the value as needed
+      } else {
+        // Scroll up
+        document.querySelector(".navbar").style.top = "0";
+      }
+      setLastScrollTop(st <= 0 ? 0 : st);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollTop]);
   return (
     <>
-          <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <div class="container-fluid">
-        {/* <img src={} alt=""/> */}
-    <a class="navbar-brand" href="#">MQ'S FLIGHT</a>
+      <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container-fluid">
+          {/* <img src={} alt=""/> */}
+          <a class="navbar-brand" href="#">
+            <FontAwesomeIcon icon={faPlane} /> MQ'S FLIGHT
+          </a>
 
-  
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar"
+            aria-controls="offcanvasDarkNavbar"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">MQ'S FLIGHT</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item m-3">
-            <Link type="button" class="nav-link active btn btn-outline-light text-dark" aria-current="page" to="/">Home</Link>
-          </li>
-          <li class="nav-item m-3 dropdown">
-          <Link type="button" class="nav-link active btn btn-outline-light text-dark" data-bs-toggle="modal" data-bs-target="#checkinModal">
-             Check In
-            </Link>
-
-
-<div class="modal fade bg-secondary" id="checkinModal" tabindex="-1" aria-labelledby="checkinModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg modal-fullscreen-sm-down">
-    <div class="modal-content bg-dark">
-      <div class="modal-header">
-        <h1 class="modal-title fs-4" id="checkinModalLabel">CHECK IN</h1>
-        <button type="button" class="btn-close bg-danger" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body ">
-      <div class="form-floating text-dark mb-3">
-      <input type="text" class="form-control" id="floatingInput" placeholder="Boooking Ref."/>
-  <label for="floatingInut">Booking Reference</label>
-</div>
-<div class="form-floating text-dark ">
-<input type="email" class="form-control" id="floatingInput" placeholder="address@gmail.com"/>
-  <label for="floatingInput">Contact Email</label>
- 
-</div>
-<div class="d-grid gap-2 col-6 mx-auto">
-  <Link type="button" class="btn btn-success mt-5" to="/Checkin">
-        Proceed
-        </Link>
-</div>
-
-      </div>
-   
-    </div>
-  </div>
-</div>
-          </li>
-          <li class="nav-item m-3 dropdown">
-      
-            <Link type="button" class="nav-link active btn btn-outline-light text-dark" data-bs-toggle="modal" data-bs-target="#manageBooking">
-                My Bookings
-            </Link>
-
-
-<div class="modal fade bg-secondary" id="manageBooking" tabindex="-1" aria-labelledby="bookingModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg modal-fullscreen-sm-down">
-    <div class="modal-content bg-dark">
-      <div class="modal-header">
-        <h1 class="modal-title fs-4" id="bookingModalLabel">Manage my Booking.</h1>
-        <button type="button" class="btn-close bg-danger" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body ">
-      <h2 class="modal-title fs-5" id="exampleModalLabel">Enter your flight details to view your booking.</h2>
-      <p class="blockquote-footer mt-3">Please input your 6 digit Booking Reference and email address used on your booking to retrieve your current flight information</p>
-      <div class="form-floating text-dark mb-3">
-      <input type="text" class="form-control" id="floatingInput" placeholder="Boooking Ref."/>
-  <label for="floatingInut">Booking Reference</label>
-</div>
-<div class="form-floating text-dark">
-<input type="email" class="form-control" id="floatingInput" placeholder="address@gmail.com"/>
-  <label for="floatingInput">Contact Email</label>
-
-</div>
-<div class="modal-footer mx-auto">
-<button type="button" class="btn btn-secondary col-2" data-bs-dismiss="modal">Back</button>
-        <button type="button" class="btn btn-success col-3">Confirm</button>
-</div>
-      </div>
-    </div>
-  </div>
-</div>
-          </li>
-          <li class="nav-item m-3 dropdown dropstart">
-             <a class="nav-link dropdown-toggle border rounded" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Contact Us
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark">
-              <li>
-              <div class="col mx-auto rounded p-5">
-                      <div class="card-body text-center">
-                        <h3 class="col-p slider-head">Contact Details</h3>
-                        <p>Feel free to contact us with questions</p>
-                        <div>
-                          <b>Email:</b>wuesemimi@gmail.com
+          <div
+            class="offcanvas offcanvas-end text-bg-dark"
+            tabindex="-1"
+            id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel"
+          >
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                MQ'S FLIGHT
+              </h5>
+              <button
+                type="button"
+                class="btn-close btn-close-white"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item m-3">
+                  <Link
+                    type="button"
+                    class="nav-link active btn btn-outline-light text-dark"
+                    aria-current="page"
+                    to="/"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item m-3 dropdown">
+                  <button
+                    type="button"
+                    className="nav-link active btn btn-outline-light text-dark"
+                    data-bs-toggle="modal"
+                    data-bs-target="#checkinModal"
+                  >
+                    Check In
+                  </button>
+                  <div
+                    className="modal fade bg-secondary modal-bg"
+                    id="checkinModal"
+                    tabIndex="-1"
+                    aria-labelledby="checkinModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog modal-lg modal-fullscreen-sm-down">
+                      <div className="modal-content bg-dark">
+                        <div className="modal-header">
+                          <h1
+                            className="modal-title fs-4"
+                            id="checkinModalLabel"
+                          >
+                            CHECK IN
+                          </h1>
+                          <button
+                            type="button"
+                            className="btn-close bg-danger"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                          ></button>
                         </div>
-                        <a
-                          href=" https://twitter.com/_i2reality?t=M9P-c8cdmekEYKiC-zuMiw&s=09"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                         <img width="48" height="48" src="https://img.icons8.com/color/48/twitter--v1.png" alt="twitter--v1"/>
-                        </a>
-                        <a
-                          href=""
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <img width="48" height="48" src="https://img.icons8.com/color/48/tiktok--v1.png" alt="tiktok--v1"/>
-                        </a>
-                        <Link
-                          to=" https://instagram.com/i2_reality?igshid=MzRlODBiNWFlZA=="
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                         <img width="48" height="48" src="https://img.icons8.com/color/48/instagram-new--v1.png" alt="instagram-new--v1"/>
-                        </Link>
+                        <div className="modal-body">
+                          <div className="form-floating text-dark mb-3">
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="bookingRefInput"
+                              placeholder="Booking Reference"
+                              value={bookingRef}
+                              onChange={handleBookingRefChange}
+                            />
+                            <label htmlFor="bookingRefInput">
+                              Booking Reference
+                            </label>
+                          </div>
+                          <div className="form-floating text-dark mb-3">
+                            <input
+                              type="email"
+                              className="form-control"
+                              id="emailInput"
+                              placeholder="Email"
+                              value={contactEmail}
+                              onChange={handleContactEmailChange}
+                            />
+                            <label htmlFor="emailInput">Contact Email</label>
+                          </div>
+                          <div className="d-grid gap-2 col-6 mx-auto">
+                            <button
+                              className="btn btn-success mt-5"
+                              onClick={() =>
+                                console.log(
+                                  "Proceed clicked with bookingRef:",
+                                  bookingRef,
+                                  "and email:",
+                                  contactEmail
+                                )
+                              }
+                            >
+                              Proceed
+                            </button>
+                          </div>
+                        </div>
                       </div>
                     </div>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-        </nav>
+                  </div>
+                </li>
+                <li class="nav-item m-3 dropdown dropstart">
+                  <a
+                    class="nav-link dropdown-toggle border rounded"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Contact Us
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-dark">
+                    <li>
+                      <div class="col mx-auto rounded p-5">
+                        <div class="card-body text-center">
+                          <h3 class="col-p slider-head">Contact Details</h3>
+                          <p>Feel free to contact us with questions</p>
+                          <div>
+                            <b>Email:</b>wuesemimi@gmail.com
+                          </div>
+                          <a
+                            href=" https://twitter.com/_i2reality?t=M9P-c8cdmekEYKiC-zuMiw&s=09"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <img
+                              width="48"
+                              height="48"
+                              src="https://img.icons8.com/color/48/twitter--v1.png"
+                              alt="twitter--v1"
+                            />
+                          </a>
+                          <a href="" target="_blank" rel="noopener noreferrer">
+                            <img
+                              width="48"
+                              height="48"
+                              src="https://img.icons8.com/color/48/tiktok--v1.png"
+                              alt="tiktok--v1"
+                            />
+                          </a>
+                          <Link
+                            to=" https://instagram.com/i2_reality?igshid=MzRlODBiNWFlZA=="
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <img
+                              width="48"
+                              height="48"
+                              src="https://img.icons8.com/color/48/instagram-new--v1.png"
+                              alt="instagram-new--v1"
+                            />
+                          </Link>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
